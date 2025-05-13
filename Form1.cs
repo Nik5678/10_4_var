7946d54d1603201d4429651693a634c7d8d38753
Nik5678 <60752699+Nik5678@users.noreply.github.com>
2025-05-14 01:20:46 +0400

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -16,29 +16,23 @@ namespace _10_4_вариант
             Graphics g = e.Graphics;
             g.SmoothingMode = SmoothingMode.AntiAlias;
 
-            // Создание перьев с разными стилями
             Pen solidPen = new Pen(Color.Red, 3);
             Pen dashPen = new Pen(Color.Blue, 3) { DashStyle = DashStyle.Dash };
             Pen dotPen = new Pen(Color.Green, 3) { DashStyle = DashStyle.Dot };
 
-            // Создание кистей
             SolidBrush blueBrush = new SolidBrush(Color.CornflowerBlue);
             SolidBrush yellowBrush = new SolidBrush(Color.Gold);
 
-            // Рисование линий
-            g.DrawLine(solidPen, 50, 50, 250, 50);      // Сплошная линия
-            g.DrawLine(dashPen, 50, 100, 250, 100);     // Пунктирная линия
-            g.DrawLine(dotPen, 50, 150, 250, 150);      // Точечная линия
+            g.DrawLine(solidPen, 50, 50, 250, 50);
+            g.DrawLine(dashPen, 50, 100, 250, 100);
+            g.DrawLine(dotPen, 50, 150, 250, 150);
 
-            // Рисование прямоугольников
-            g.DrawRectangle(solidPen, new Rectangle(300, 50, 150, 100));  // Незакрашенный
-            g.FillRectangle(blueBrush, new Rectangle(300, 200, 150, 100)); // Закрашенный
+            g.DrawRectangle(solidPen, new Rectangle(300, 50, 150, 100));
+            g.FillRectangle(blueBrush, new Rectangle(300, 200, 150, 100));
 
-            // Рисование эллипсов
-            g.DrawEllipse(dashPen, new Rectangle(500, 50, 150, 100));      // Незакрашенный
-            g.FillEllipse(yellowBrush, new Rectangle(500, 200, 150, 100)); // Закрашенный
+            g.DrawEllipse(dashPen, new Rectangle(500, 50, 150, 100));
+            g.FillEllipse(yellowBrush, new Rectangle(500, 200, 150, 100));
 
-            // Рисование многоугольника
             Point[] polygonPoints = {
                 new Point(100, 400),
                 new Point(200, 350),
@@ -46,10 +40,9 @@ namespace _10_4_вариант
                 new Point(250, 500),
                 new Point(150, 500)
             };
-            g.DrawPolygon(dotPen, polygonPoints);       // Незакрашенный
-            g.FillPolygon(blueBrush, polygonPoints);     // Закрашенный
+            g.DrawPolygon(dotPen, polygonPoints);
+            g.FillPolygon(blueBrush, polygonPoints);
 
-            // Освобождение ресурсов
             solidPen.Dispose();
             dashPen.Dispose();
             dotPen.Dispose();
